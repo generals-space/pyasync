fae211e76c234724df8dec07eb7a512f89acda73
general <generals.space@gmail.com>
2018-09-23 17:20:54 +0800

    协程队列示例添加超时时间

--- 05. 动态添加任务改进(队列)/readme.md
+++ 05. 动态添加任务改进(队列)/readme.md
@@ -3,3 +3,13 @@
 之后在研究tornado时, 在ta的官方文档中得到了灵感, 就是示例04的代码. 
 
 动态添加任务不一定要开线程, 使用异步队列, 和两个循环可以模拟生产者和消费者模型. 这个示例就是对示例03的改进.
+
+------
+
+`task_queue_timeout.py`还为每个任务添加了超时时间的限制. 因为`run_coroutine_threadsafe()`函数不能设置超时, 所以把`fetch_url`协程用`wait_for()`函数包装了一下, 完美.
+
+`gather(coroutine1, coroutine2...])`: 同时执行多个协程函数, 每个协程算是一个参数;
+
+`wait([coroutine1, coroutine2...], timeout)`: 与`gather`一样, 同时执行多个协程函数, 不过协程函数是以列表形式传入的, 还可以设置超时时间;
+
+`wait_for(coroutine, timeout)`: 执行单个协程函数, 也可以设置超时.
---
+++ 05. 动态添加任务改进(队列)/task_queue_timeout.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import asyncio
+import time
+from aiohttp import ClientSession
+
+url = 'https://note.generals.space/aio'
+task_queue = asyncio.Queue()
+
+async def fetch_url(session, order, url):
+    print('fetching...', order)
+    res = await session.get(url)
+    resText = await res.json()
+    print('order %d: %s' % (order, resText))
+    return resText
+
+async def fetch_url_timeout(session, order, url, loop = None, timeout = 10):
+    try:
+        await asyncio.wait_for(fetch_url(session, order, url), timeout)
+    except asyncio.TimeoutError:
+        print(f'task {order} timeout')
+    else:
+        print(f'task {order} complete')
+
+async def customer(loop):
+    print('customer start...')
+    aioSession = ClientSession(loop = loop)
+    while True:
+        _order, _url = await task_queue.get()
+        asyncio.run_coroutine_threadsafe(fetch_url_timeout(aioSession, _order, _url, loop = loop, timeout = 10), loop)
+        ## asyncio.run_coroutine_threadsafe(fetch_url(aioSession, _order, _url), loop)
+
+    print('customer complete...')
+
+async def producer():
+    print('producer start...')
+    for i in range(10):
+        ## await asyncio.sleep(2)
+        await task_queue.put((i, url))
+
+    print('producer complete...')
+
+async def main(loop):
+    co = [producer(), customer(loop)]
+    await asyncio.wait(co)
+    print('主线程结束...')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop))
+
+# producer start...
+# producer complete...
+# customer start...
+# fetching... 0
+# ...
+# fetching... 9
+# order 5: {'delay': 3}
+# task 5 complete
+# order 7: {'delay': 8}
+# order 9: {'delay': 6}
+# task 7 complete
+# task 9 complete
+# task 0 timeout
+# ...
+# task 8 timeout
