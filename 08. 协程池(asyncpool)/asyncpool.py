82c105b1c29b925ff203da3ccc157cb8cd3f16d1
general <generals.space@gmail.com>
2018-09-26 20:26:50 +0800

    移除logger

--- 08. 协程池(asyncpool)/asyncpool.py
+++ 08. 协程池(asyncpool)/asyncpool.py
@@ -10,7 +10,7 @@ class Terminator:
     pass
 
 class AsyncPool:
-    def __init__(self, loop, num_workers: int, logger_name: str, worker_co, load_factor: int = 1,
+    def __init__(self, loop, num_workers: int, worker_co, load_factor: int = 1,
                  job_accept_duration: int = None, max_task_time: int = None, return_futures: bool = False,
                  raise_on_join: bool = False, log_every_n: int = None, expected_total=None):
         """
@@ -21,7 +21,6 @@ class AsyncPool:
         worker_co, self.exceptions will be set to True.
         @param loop: asyncio loop to use
         @param num_workers: number of async tasks which will pull from the internal queue
-        @param logger_name: logger_name of the worker pool (used for logging)
         @param worker_co: async coroutine to call when an item is retrieved from the queue
         @param load_factor: multiplier used for number of items in queue
         @param job_accept_duration: maximum number of seconds from first push to last push before a TimeoutError will be thrown.
@@ -30,14 +29,12 @@ class AsyncPool:
             Set to None for no limit.
         @param return_futures: set to reture to return a future for each `push` (imposes CPU overhead)
         @param raise_on_join: raise on join if any exceptions have occurred, default is False
-        @param log_every_n: (optional) set to number of `push`s each time a log statement should be printed (default does not print every-n pushes)
         @param expected_total: (optional) expected total number of jobs (used for `log_event_n` logging)
         @return: instance of AsyncWorkerPool
         """
         loop = loop or asyncio.get_event_loop()
         self._loop = loop
         self._num_workers = num_workers
-        self._logger = logging.getLogger(logger_name)
         ## 参数队列, 也即任务队列.
         self._queue = asyncio.Queue(num_workers * load_factor)
         self._workers = None
@@ -47,10 +44,7 @@ class AsyncPool:
         self._max_task_time = max_task_time
         self._return_futures = return_futures
         self._raise_on_join = raise_on_join
-        self._logger_name = logger_name
         self._worker_co = worker_co
-        self._total_queued = 0
-        self._log_every_n = log_every_n
         self._expected_total = expected_total
 
     async def _worker_loop(self):
@@ -84,7 +78,7 @@ class AsyncPool:
                     # don't log the failure when the client is receiving the future
                     future.set_exception(e)
                 else:
-                    self._logger.exception('Worker call failed')
+                    print('Worker call failed')
             finally:
                 if got_obj:
                     self._queue.task_done()
@@ -92,17 +86,6 @@ class AsyncPool:
     @property
     def exceptions(self):
         return self._exceptions
-
-    @property
-    def total_queued(self):
-        return self._total_queued
-
-    async def __aenter__(self):
-        self.start()
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.join()
 
     async def push(self, *args, **kwargs) -> asyncio.Future:
         """ Method to push work to `worker_co` passed to `__init__`.
@@ -113,14 +96,10 @@ class AsyncPool:
             self._first_push_dt = utc_now()
 
         if self._job_accept_duration is not None and (utc_now() - self._first_push_dt) > self._job_accept_duration:
-            raise TimeoutError("Maximum lifetime of {} seconds of AsyncWorkerPool: {} exceeded".format(self._job_accept_duration, self._logger_name))
+            raise TimeoutError("Maximum lifetime of {} seconds of AsyncWorkerPool exceeded".format(self._job_accept_duration))
 
         future = asyncio.futures.Future(loop=self._loop) if self._return_futures else None
         await self._queue.put((future, args, kwargs))
-        self._total_queued += 1
-
-        if self._log_every_n is not None and (self._total_queued % self._log_every_n) == 0:
-            self._logger.info("pushed {}/{} items to {} AsyncWorkerPool".format(self._total_queued, self._expected_total, self._logger_name))
 
         return future
 
@@ -137,7 +116,6 @@ class AsyncPool:
         if not self._workers:
             return
 
-        self._logger.debug('Joining {}'.format(self._logger_name))
         # The Terminators will kick each worker from being blocked against the _queue.get() and allow
         # each one to exit
         for _ in range(self._num_workers):
@@ -146,11 +124,9 @@ class AsyncPool:
         try:
             await asyncio.gather(*self._workers, loop=self._loop)
             self._workers = None
-        except:
-            self._logger.exception('Exception joining {}'.format(self._logger_name))
+        except Exception as e:
+            print('Exception joining ', e)
             raise
-        finally:
-            self._logger.debug('Completed {}'.format(self._logger_name))
 
         if self._exceptions and self._raise_on_join:
-            raise Exception("Exception occurred in pool {}".format(self._logger_name))
+            raise Exception("Exception occurred in pool")
--- 08. 协程池(asyncpool)/main.py
+++ 08. 协程池(asyncpool)/main.py
@@ -3,12 +3,14 @@
 import asyncpool
 import logging
 import asyncio
-import sys
+from aiohttp import ClientSession
 
-async def worker(initial_number, result_queue):
-    print("Processing Value! -> {} * 2 = {}".format(initial_number, initial_number * 2))
-    await asyncio.sleep(1)
-    await result_queue.put(initial_number * 2)
+async def fetch_url(order, url, result_queue):
+    session = ClientSession(loop=loop)
+    print('fetching...', order)
+    res = await session.get(url)
+    resText = await res.json()
+    await result_queue.put(resText)
 
 async def result_reader(queue):
     while True:
@@ -19,29 +21,22 @@ async def result_reader(queue):
 
 async def run():
     result_queue = asyncio.Queue()
-
     reader_future = asyncio.ensure_future(result_reader(result_queue), loop=loop)
 
-    # Start a worker pool with 10 coroutines, invokes `worker` and waits for it to complete or 5 minutes to pass.
     ## 启动一个可以运行10个协程的协程池
-    async with asyncpool.AsyncPool(loop, num_workers=10, 
-                             logger_name=logger_name,
-                             worker_co=worker, max_task_time=300,
-                             log_every_n=10) as pool:
-        for i in range(50):
-            ## 这里push的是工作协程worker的参数, 
-            ## 在协程池中, 每个工作协程从队列里获取参数, 
-            ## 传给worker_co, 即worker函数去执行.
-            ## result_queue是结果队列, 工作协程执行完毕后会把结果放进去.
-            await pool.push(i, result_queue)
+    pool = asyncpool.AsyncPool(loop, num_workers=10, worker_co=worker, max_task_time=300)
+    pool.start()
 
+    for i in range(50):
+        ## 这里push的是工作协程worker的参数, 
+        ## 在协程池中, 每个工作协程从队列里获取参数, 
+        ## 传给worker_co, 即worker函数去执行.
+        ## result_queue是结果队列, 工作协程执行完毕后会把结果放进去.
+        await pool.push(i, result_queue)
+
+    await pool.join()
     await result_queue.put(None)
     await reader_future
-logger_name = 'mypool'
-formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
-logger = logging.Logger(logger_name)
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.formatter = formatter  # 也可以直接给formatter赋值
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
