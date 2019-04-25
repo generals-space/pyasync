7a48d9177d81c04ce74693e05441b546bd8a48ae
general <generals.space@gmail.com>
2019-04-26 00:26:41 +0800

    更新: asyncio call_soon_threadsafe()的应用场景

--- 12. asyncio的call_XX函数族/call.py
+++ 12. asyncio的call_XX函数族/call.py
@@ -3,7 +3,7 @@ import asyncio
 
 from aiohttp import ClientSession
 
-url = 'http://localhost:3000/aio'
+url = 'https://note.generals.space/aio'
 
 def callback(loop, order):
     print('loop time: {:f}, request {:d} to {:s} success'.format(loop.time(), order, url))
@@ -42,13 +42,13 @@ print('cost %f' % (end - start))
 
 ## 输出
 ## loop time: 371982.687000, order: 4, result: b'{"delay":4}'
-## loop time: 371982.687000, request 4 to http://localhost:3000/aio success
+## loop time: 371982.687000, request 4 to https://note.generals.space/aio success
 ## loop time: 371983.687000, order: 1, result: b'{"delay":5}'
-## loop time: 371983.687000, request 1 to http://localhost:3000/aio success
+## loop time: 371983.687000, request 1 to https://note.generals.space/aio success
 ## loop time: 371985.687000, order: 2, result: b'{"delay":7}'
-## loop time: 371985.687000, request 2 to http://localhost:3000/aio success
+## loop time: 371985.687000, request 2 to https://note.generals.space/aio success
 ## loop time: 371991.687000, order: 3, result: b'{"delay":13}'
-## loop time: 371991.687000, request 3 to http://localhost:3000/aio success
+## loop time: 371991.687000, request 3 to https://note.generals.space/aio success
 ## loop time: 371996.687000, order: 0, result: b'{"delay":18}'
-## loop time: 371996.687000, request 0 to http://localhost:3000/aio success
+## loop time: 371996.687000, request 0 to https://note.generals.space/aio success
 ## cost 18.041001
--- 12. asyncio的call_XX函数族/readme.md
+++ 12. asyncio的call_XX函数族/readme.md
@@ -1,6 +1,8 @@
 参考文章
 
-[asyncio并发编程](http://www.cnblogs.com/lyq-biu/p/10486148.html)
+1. [asyncio并发编程](http://www.cnblogs.com/lyq-biu/p/10486148.html)
+
+2. [Python黑魔法 --- 异步IO（ asyncio） 协程](https://www.jianshu.com/p/b5e347b3a17c)
 
 `asyncio`的`call_XX`函数族中的函数有如下几个
 
@@ -15,4 +17,6 @@ ta们的作用个人感觉基本类似于js中的`setTimeout()`, 都是手动设
 
 `call.py`为`call_XX`函数族的使用方法, 几乎相同.
 
-关于`call_soon_threadsafe()`的使用场景, 一直没找到合适的, `call_safe.py`是使用子线程维护协程事件循环, 然后在主线程中动态添加任务. 但是在协程任务中无论使用`call_soon()`还是`call_soon_threadsafe()`都没有报错...先这样的, 以后再找找必须使用后者的情况.
+~~ 关于`call_soon_threadsafe()`的使用场景, 一直没找到合适的, `call_safe.py.1`是使用子线程维护协程事件循环, 然后在主线程中动态添加任务. 但是在协程任务中无论使用`call_soon()`还是`call_soon_threadsafe()`都没有报错...先这样的, 以后再找找必须使用后者的情况. ~~
+
+好吧, 按照参考文章2, 又重新编写了`call_safe.py`, 在这个程序中, `call_soon()`方法的确不好使.
